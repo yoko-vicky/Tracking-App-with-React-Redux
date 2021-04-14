5ee3aa985a1b11da9a776899f5e535c927b56904
yocosaka <sakayoco55@gmail.com>
2021-04-14 10:02:53 +0900

    Add checkLoginStatus in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
 
 const App = () => {
   const [loggedInStatus, setLoggedInStatus] = useState('Not logged in');
   const [user, setUser] = useState({});
+
   const handleLogin = (data) => {
     setLoggedInStatus('Logged in!');
     setUser(data.user);
   };
+
+  const checkLoginStatus = () => {
+    axios.get('http://localhost:3001/logged_in', { withCredentials: true })
+
+      .then((response) => {
+        if (response.data.logged_in && loggedInStatus === 'Not logged in') {
+          setLoggedInStatus('Logged in!');
+          setUser(response.data.user);
+        } else if (!response.data.logged_in && loggedInStatus === 'Logged in!') {
+          setLoggedInStatus('Not logged in');
+          setUser({});
+        }
+      })
+
+      .catch((error) => {
+        console.log('Login Error: ', error);
+      });
+  };
+
+  useEffect(() => {
+    checkLoginStatus();
+  });
 
   return (
     <BrowserRouter>
