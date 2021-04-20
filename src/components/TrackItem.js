cc626dbd3e452a3645a477f643b6a531929a6ce6
yocosaka <sakayoco55@gmail.com>
2021-04-20 20:32:40 +0900

    Add path to EditTrack from TrackItem

--- src/components/TrackItem.js
+++ src/components/TrackItem.js
@@ -1,27 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import pluralize from 'pluralize';
 
 /* eslint-disable camelcase */
 const TrackItem = ({ item, targetTrack }) => {
   const {
     icon, unit, title, target,
   } = item;
-  const { result } = targetTrack;
+  const { result, date } = targetTrack;
   const achivementRate = Math.floor((result / target) * 100);
 
   return (
     <div className="items__item">
-      <div className="items__icon">
-        <span className="iconify" data-icon={icon || 'bi:pen-fill'} data-inline="false" />
-      </div>
-      <div className="items__text">
-        <div className="items__title">{title}</div>
-        <div className="items__result">
-          <span className="items__result__num">{result || '0'}</span>
-          <span className="items__result__unit">{`${unit} / ${target}`}</span>
+      <Link to={`/track/${Number(date)}/edit`} className="items__item-link">
+        <div className="items__icon">
+          <span className="iconify" data-icon={icon || 'bi:pen-fill'} data-inline="false" />
         </div>
-        <div className="items__rate">{`Achivement rate: ${achivementRate || '0'} %`}</div>
-      </div>
+        <div className="items__text">
+          <div className="items__title">{title}</div>
+          <div className="items__result">
+            <span className="items__result__num">{result || '0'}</span>
+            <span className="items__result__unit">{`${pluralize(unit, result)} / ${target}`}</span>
+          </div>
+          <div className="items__rate">{`Achivement rate: ${achivementRate || '0'} %`}</div>
+        </div>
+      </Link>
     </div>
   );
 };
