b087105979b3f38b828cc3319cb36b203a0b1bfa
yocosaka <sakayoco55@gmail.com>
2021-04-23 21:43:04 +0900

    Add authHeaders to authUsers.js

--- src/helpers/authUsers.js
+++ src/helpers/authUsers.js
@@ -3,19 +3,13 @@ import authHeaders from './authHeaders';
 import baseUrl from './baseUrl';
 
 export const signedUp = async (username, password) => {
-  const response = await axios.post(`${baseUrl}signup`, { user: { username, password } }, { withCredentials: true })
+  const response = await axios.post(`${baseUrl}signup`, { user: { username, password } }, authHeaders())
     .then((response) => response.data).catch((error) => error);
   return response;
 };
 
 export const loggedIn = async (username, password) => {
-  const response = await axios.post(`${baseUrl}login`, { user: { username, password } }, { withCredentials: true })
-    .then((response) => response.data).catch((error) => error);
-  return response;
-};
-
-export const autoLogIn = async () => {
-  const response = await axios.get(`${baseUrl}auto_login`, authHeaders())
+  const response = await axios.post(`${baseUrl}login`, { user: { username, password } }, authHeaders())
     .then((response) => response.data).catch((error) => error);
   return response;
 };
