40e1deff91e83cd56ba45d719aec523c47fead79
yocosaka <sakayoco55@gmail.com>
2021-05-01 19:29:48 +0900

    Fix baseUrl path for production

--- src/helpers/baseUrl.js
+++ src/helpers/baseUrl.js
@@ -1,4 +1,4 @@
 // const baseUrl = 'http://localhost:3001/';
-const baseUrl = 'https://yoco-tracking-app-api.herokuapp.com/';
+const baseUrl = 'https://yoco-tracking-app-api.herokuapp.com';
 
 export default baseUrl;
