f620617d3e5e119bfcd23ee64036521c300e7ae5
yocosaka <sakayoco55@gmail.com>
2021-05-04 11:11:52 +0900

    Fix path to login and auto_login in authUsers.js

--- src/helpers/authUsers.js
+++ src/helpers/authUsers.js
@@ -1,7 +1,7 @@
 import { sendRequestWithData } from './api';
 
 export const signedUp = async (username, password) => {
-  const response = await sendRequestWithData('post', 'signup', { user: { username, password } })
+  const response = await sendRequestWithData('post', 'users', { user: { username, password } })
     .then((response) => response.data).catch((error) => error);
   return response;
 };
