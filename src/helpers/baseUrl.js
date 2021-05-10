881c85c7a4bba41dde3a038198c468ecf489e631
yocosaka <sakayoco55@gmail.com>
2021-05-10 13:42:58 +0900

    Update baseUrl with api/v1

--- src/helpers/baseUrl.js
+++ src/helpers/baseUrl.js
@@ -1,4 +1,4 @@
-// const baseUrl = 'http://localhost:3001/';
-const baseUrl = 'https://yoco-tracking-app-api.herokuapp.com';
+// const baseUrl = 'http://localhost:3001/api/v1';
+const baseUrl = 'https://yoco-tracking-app-api.herokuapp.com/api/v1/';
 
 export default baseUrl;
